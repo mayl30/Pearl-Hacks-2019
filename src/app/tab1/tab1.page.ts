898d2bfb24e3eefdf4ea936362be5b7ae8fc804a
chantalshine <34609430+cshine6008@users.noreply.github.com>
2019-02-16 19:58:11 -0500

    camera stuff added

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Window } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  myphoto: any;
+  window: Window;
+
+  constructor(private camera: Camera) {
+
+   }
+
+   openCamera() {
+    const options: CameraOptions = {
+      quality: 70,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      targetWidth: 400,
+      targetHeight: 400,
+      saveToPhotoAlbum: true,
+      correctOrientation: true
+    };
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.myphoto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+   }
+}
